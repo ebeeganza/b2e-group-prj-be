e5b84b6ec2c8c48b9fd82f2f93a2fae951d5ee95
York MacBook 090 <yorkmacbook090@York-MacBook-090.local>
2022-12-29 14:35:18 -0600

    created controller, repository, and service for category

---
+++ src/main/java/net/yorksolutions/optumfsjavadukesofyork2/controllers/CategoryController.java
@@ -0,0 +1,2 @@
+package net.yorksolutions.optumfsjavadukesofyork2.controllers;public class CategoryController {
+}
---
+++ src/main/java/net/yorksolutions/optumfsjavadukesofyork2/repositories/CategoryRepository.java
@@ -0,0 +1,6 @@
+package net.yorksolutions.optumfsjavadukesofyork2.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Category extends CrudRepository {
+}
