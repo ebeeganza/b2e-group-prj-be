9555a1bed8c3f71958ce26c87eb4e438f19fdc56
ssawtelle <ssawtelle10@gmail.com>
2022-12-29 13:33:40 -0600

    User Skeleton

--- src/main/java/net/yorksolutions/optumfsjavadukesofyork2/controllers/AppUserController.java
+++ src/main/java/net/yorksolutions/optumfsjavadukesofyork2/controllers/AppUserController.java
@@ -1,4 +1,14 @@
 package net.yorksolutions.optumfsjavadukesofyork2.controllers;
 
+import net.yorksolutions.optumfsjavadukesofyork2.services.AppUserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin
+@RestController
+@RequestMapping("/users")
 public class AppUserController {
+    private AppUserService appUserService;
+
 }
