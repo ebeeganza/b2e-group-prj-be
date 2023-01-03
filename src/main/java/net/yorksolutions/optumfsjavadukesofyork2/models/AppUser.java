b00365611e97d97d6496219dc260e2ab3da80bda
ssawtelle <ssawtelle10@gmail.com>
2023-01-03 13:43:10 -0600

    fname-lname added

--- src/main/java/net/yorksolutions/optumfsjavadukesofyork2/models/AppUser.java
+++ src/main/java/net/yorksolutions/optumfsjavadukesofyork2/models/AppUser.java
@@ -7,17 +7,11 @@ public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     public Long id;
+    public String fname;
+    public String lname;
     public String username;
     public String password;
     public Integer role;
 
-    public AppUser() {}
-
-    public AppUser(Long id, String username, String password, Integer role) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.role = role;
-    }
 
 }
